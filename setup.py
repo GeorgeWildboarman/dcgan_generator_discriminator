68b1df48b52fdc87b4e42853b14f50ee98bfad3b
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2023-05-16 08:20:29 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dcgan_generator_discriminator',
+    name='gan_generator_discriminator',
     version='1.0.0',
+    author='GeorgeWildboarman',
 #     url='http://github.com/GeorgeWildboarman/dcgan-generator-discriminator',
 #     download_url='http://github.com/GeorgeWildboarman/dcgan-generator-discriminator',
     packages=find_packages(),
